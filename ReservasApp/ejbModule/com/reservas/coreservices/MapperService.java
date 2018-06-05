a512eabcdbc7fc1413b8321cb150d915f038b0e5
Walter <Walter@192.168.0.10>
2018-06-05 00:29:28 -0300

    clase del 4-6

---
+++ ReservasApp/ejbModule/com/reservas/coreservices/MapperService.java
@@ -0,0 +1,10 @@
+package com.reservas.coreservices;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+@Stateless
+@LocalBean
+public class MapperService implements MapperServiceInterfaceLocal{
+
+}
