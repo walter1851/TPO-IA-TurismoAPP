065a102a449bd9415100f7bf25fc5641e2a51504
Walter <Walter@192.168.0.10>
2018-06-05 00:52:11 -0300

    clase del jueves 4-6

--- ReservasApp/ejbModule/com/reservas/businessdelegate/BusinessDelegate.java
+++ ReservasApp/ejbModule/com/reservas/businessdelegate/BusinessDelegate.java
@@ -7,7 +7,8 @@ import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
-import com.reservas.controller.Controller;
+import com.reservas.controller.ControllerBusqueda;
+import com.reservas.controller.ControllerReserva;
 import com.reservas.dto.MedioPagoDTO;
 import com.reservas.dto.OfertaDTO;
 
@@ -15,23 +16,26 @@ import com.reservas.dto.OfertaDTO;
 @LocalBean
 public class BusinessDelegate implements BusinessDelegateInterfaceLocal{
 	@EJB
-	Controller controller;
+	ControllerReserva controllerReserva;
+	@EJB
+	ControllerBusqueda controllerBusqueda;
+	
 	public void reservarHotel(int ofertaid, String fDesde, String fHasta, String tipoHabitacion, int cantPersonas,
 			String nombre, String apellido, String dni, MedioPagoDTO medioPagoDTO) {
-		controller.reservarHotel(ofertaid, fDesde, fHasta, tipoHabitacion, cantPersonas, nombre, apellido, dni, medioPagoDTO);
+		controllerReserva.reservarHotel(ofertaid, fDesde, fHasta, tipoHabitacion, cantPersonas, nombre, apellido, dni, medioPagoDTO);
 	}
 
 	public void reservarPaquete(int ofertaid, String fDesde, String fHasta, int cantPersonas, String nombre,
 			String apellido, String dni, MedioPagoDTO medioPagoDTO) {
-		controller.reservarPaquete(ofertaid, fDesde, fHasta, cantPersonas, nombre, apellido, dni, medioPagoDTO);
+		controllerReserva.reservarPaquete(ofertaid, fDesde, fHasta, cantPersonas, nombre, apellido, dni, medioPagoDTO);
 	}
 
 	public List<OfertaDTO> buscarOfertaHotelera(String destino, int cantPersonas, String fDesde, String fHasta,
 			String tipoHabitacion) throws ParseException {
-		return controller.buscarOfertaHotelera(destino, cantPersonas, fDesde, fHasta, tipoHabitacion);
+		return controllerBusqueda.buscarOfertaHotelera(destino, cantPersonas, fDesde, fHasta, tipoHabitacion);
 	}
 
 	public List<OfertaDTO> buscarOfertaPaquete(String destino, int cantPersonas, String fDesde, String fHasta) throws ParseException {
-		return controller.buscarOfertaPaquete(destino, cantPersonas, fDesde, fHasta);
+		return controllerBusqueda.buscarOfertaPaquete(destino, cantPersonas, fDesde, fHasta);
 	}
 }
--- ReservasApp/ejbModule/com/reservas/controller/ControllerReserva.java
+++ ReservasApp/ejbModule/com/reservas/controller/ControllerReserva.java
@@ -14,25 +14,14 @@ import com.reservas.dto.OfertaDTO;
 
 @Stateless
 @LocalBean
-public class Controller implements ControllerInterfaceLocal{
-	@EJB
-	BusquedaService busquedaOfertaService;
+public class ControllerReserva implements ControllerReservaInterfaceLocal{
 	@EJB
 	ReservaService reservaService;
 
 	public void reservarHotel(int ofertaid,String fDesde,String fHasta,String tipoHabitacion,int cantPersonas,String nombre,String apellido,String dni,MedioPagoDTO medioPagoDTO) {
 		reservaService.reservarHotel(ofertaid, fDesde, fHasta, tipoHabitacion, cantPersonas, nombre, apellido, dni, medioPagoDTO);
 	}
-
 	public void reservarPaquete(int ofertaid,String fDesde,String fHasta,int cantPersonas,String nombre,String apellido,String dni,MedioPagoDTO medioPagoDTO) {
 		reservaService.reservarPaquete(ofertaid, fDesde, fHasta, cantPersonas, nombre, apellido, dni, medioPagoDTO);
-	}
-
-	public List<OfertaDTO> buscarOfertaPaquete(String destino,int cantPersonas,String fDesde, String fHasta) throws ParseException{
-		return busquedaOfertaService.buscarOfertaPaquete(destino, cantPersonas, fDesde, fHasta);
-	}
-
-	public List<OfertaDTO> buscarOfertaHotelera(String destino,int cantPersonas,String fDesde, String fHasta, String tipoHabitacion) throws ParseException {
-		return busquedaOfertaService.buscarOfertaHotelera(destino, cantPersonas, fDesde, fHasta, tipoHabitacion);
 	}
 }
