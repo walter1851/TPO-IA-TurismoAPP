5dc7a1f47156cdc5f346ea2cefa4edc3dd4da7a9
Walter <Walter@192.168.0.10>
2018-06-14 23:26:56 -0300

    Codeando paquete de integracion

--- TurismoApp/ejbModule/com/turismo/integraciones/qconsumer/OfertaHoteleraMensaje.java
+++ TurismoApp/ejbModule/com/turismo/integraciones/qconsumer/OfertaHoteleraMensaje.java
@@ -4,31 +4,227 @@ import java.util.Date;
 import java.util.List;
 
 public class OfertaHoteleraMensaje {
-	private int id;
-	private String nombreOferta;
+	// Campos oferta hotelera
+	private int idOfertaHotelera;
+	private String nombreOfertaHotelera;
 	private float precio;// precio de la habitacion
-	private int cupo; 
-	private String mediosDePago;
+	private int cupo;
+	// FALTA CANT PERSONAS?
+	private List<String> mediosDePago;
 	private String tipoHabitacion; // SIMPLE, DOBLE, TRIPLE
-	//Establecimiento
+	// Establecimiento
 	private int idEstablecimiento;
 	private String uidBackOffice; // Id recibido del backoffice
-	private String nombre;
-	private String direccion;
+	private String nombreEstablecimiento;
+	private String direccionEstablecimiento;
 	private int idCiudad;
 	private String nombreCiudad;
+	// Hotel
 	private int idHotel;
 	private String nombreHotel;
 	private String urlFotoHotel;
-	private String descripcionHotel;
+	// Establecimiento
+	private String descripcionEstablecimiento;
 	private String mapaLatitud;
 	private String mapaLongitud;
-	private String urlFotoEstablecimiento;//Esto es una foto sola, no es un array
+	private String urlFotoEstablecimiento;// Esto es una foto sola, no es un array
 	private int cantEstrellas;// de 1 a 5
-	//ContMensj Hot
+	// Campos oferta hotelera
 	private Date fechaDesde;// Ej: 2007-04-05T12:30-02:00
 	private Date fechaHasta;// Ej: 2007-04-05T12:30-02:00
-	private List<String> politicas;// Texto con las politicas
-	private String servicios;
+	private String politicaCancelacion;// Texto con las politicas
+	private List<String> servicios;
+
+	public int getIdOfertaHotelera() {
+		return idOfertaHotelera;
+	}
+
+	public void setIdOfertaHotelera(int idOfertaHotelera) {
+		this.idOfertaHotelera = idOfertaHotelera;
+	}
+
+	public String getNombreOfertaHotelera() {
+		return nombreOfertaHotelera;
+	}
+
+	public void setNombreOfertaHotelera(String nombreOfertaHotelera) {
+		this.nombreOfertaHotelera = nombreOfertaHotelera;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public int getCupo() {
+		return cupo;
+	}
+
+	public void setCupo(int cupo) {
+		this.cupo = cupo;
+	}
+
+	public List<String> getMediosDePago() {
+		return mediosDePago;
+	}
+
+	public void setMediosDePago(List<String> mediosDePago) {
+		this.mediosDePago = mediosDePago;
+	}
+
+	public String getTipoHabitacion() {
+		return tipoHabitacion;
+	}
+
+	public void setTipoHabitacion(String tipoHabitacion) {
+		this.tipoHabitacion = tipoHabitacion;
+	}
+
+	public int getIdEstablecimiento() {
+		return idEstablecimiento;
+	}
+
+	public void setIdEstablecimiento(int idEstablecimiento) {
+		this.idEstablecimiento = idEstablecimiento;
+	}
+
+	public String getUidBackOffice() {
+		return uidBackOffice;
+	}
+
+	public void setUidBackOffice(String uidBackOffice) {
+		this.uidBackOffice = uidBackOffice;
+	}
+
+	public String getNombreEstablecimiento() {
+		return nombreEstablecimiento;
+	}
+
+	public void setNombreEstablecimiento(String nombreEstablecimiento) {
+		this.nombreEstablecimiento = nombreEstablecimiento;
+	}
+
+	public String getDireccionEstablecimiento() {
+		return direccionEstablecimiento;
+	}
+
+	public void setDireccionEstablecimiento(String direccionEstablecimiento) {
+		this.direccionEstablecimiento = direccionEstablecimiento;
+	}
+
+	public int getIdCiudad() {
+		return idCiudad;
+	}
+
+	public void setIdCiudad(int idCiudad) {
+		this.idCiudad = idCiudad;
+	}
+
+	public String getNombreCiudad() {
+		return nombreCiudad;
+	}
+
+	public void setNombreCiudad(String nombreCiudad) {
+		this.nombreCiudad = nombreCiudad;
+	}
+
+	public int getIdHotel() {
+		return idHotel;
+	}
+
+	public void setIdHotel(int idHotel) {
+		this.idHotel = idHotel;
+	}
+
+	public String getNombreHotel() {
+		return nombreHotel;
+	}
+
+	public void setNombreHotel(String nombreHotel) {
+		this.nombreHotel = nombreHotel;
+	}
+
+	public String getUrlFotoHotel() {
+		return urlFotoHotel;
+	}
+
+	public void setUrlFotoHotel(String urlFotoHotel) {
+		this.urlFotoHotel = urlFotoHotel;
+	}
+
+	public String getDescripcionEstablecimiento() {
+		return descripcionEstablecimiento;
+	}
+
+	public void setDescripcionEstablecimiento(String descripcionEstablecimiento) {
+		this.descripcionEstablecimiento = descripcionEstablecimiento;
+	}
+
+	public String getMapaLatitud() {
+		return mapaLatitud;
+	}
+
+	public void setMapaLatitud(String mapaLatitud) {
+		this.mapaLatitud = mapaLatitud;
+	}
+
+	public String getMapaLongitud() {
+		return mapaLongitud;
+	}
+
+	public void setMapaLongitud(String mapaLongitud) {
+		this.mapaLongitud = mapaLongitud;
+	}
+
+	public String getUrlFotoEstablecimiento() {
+		return urlFotoEstablecimiento;
+	}
+
+	public void setUrlFotoEstablecimiento(String urlFotoEstablecimiento) {
+		this.urlFotoEstablecimiento = urlFotoEstablecimiento;
+	}
+
+	public int getCantEstrellas() {
+		return cantEstrellas;
+	}
+
+	public void setCantEstrellas(int cantEstrellas) {
+		this.cantEstrellas = cantEstrellas;
+	}
+
+	public Date getFechaDesde() {
+		return fechaDesde;
+	}
+
+	public void setFechaDesde(Date fechaDesde) {
+		this.fechaDesde = fechaDesde;
+	}
+
+	public Date getFechaHasta() {
+		return fechaHasta;
+	}
+
+	public void setFechaHasta(Date fechaHasta) {
+		this.fechaHasta = fechaHasta;
+	}
+
+	public String getPoliticaCancelacion() {
+		return politicaCancelacion;
+	}
+
+	public void setPoliticaCancelacion(String politicaCancelacion) {
+		this.politicaCancelacion = politicaCancelacion;
+	}
+
+	public List<String> getServicios() {
+		return servicios;
+	}
+
+	public void setServicios(List<String> servicios) {
+		this.servicios = servicios;
+	}
 
 }
--- TurismoApp/ejbModule/com/turismo/integraciones/qconsumer/OfertaPaqueteMensaje.java
+++ TurismoApp/ejbModule/com/turismo/integraciones/qconsumer/OfertaPaqueteMensaje.java
@@ -1,22 +1,181 @@
 package com.turismo.integraciones.qconsumer;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class OfertaPaqueteMensaje {
-private int idPaquete;
-private String nombrePaquete;
-private int	idCiudadDestino;
-private String nombreCiudadDestino;
-private int cupo;
-private int cantPersonas;
-//agencia: Agencia
-private String foto;
-private LocalDate fechaDesde; // Ej: 2007-04-05T12:30-02:00
-private LocalDate fechaHasta; // Ej: 2007-04-05T12:30-02:00
-private String estado; // INACTIVO, ACTIVO
-private float precio;
-private String descripcion;
-private String politicas;
-private String servicios;
-private String mediosDePago;
+	// Oferta Paquete
+	private int idPaquete;
+	private String nombrePaquete;
+	private int idCiudadDestino;
+	private String nombreCiudadDestino;
+	private int cupo;
+	private int cantPersonas;
+	// AGENCIA
+	private int idAgencia;
+	private String nombreAgencia;
+	private String direccionAgencia;
+	private String estadoAgencia; // INACTIVO, ACTIVO
+	// Oferta Paquete
+	private String foto;
+	private LocalDate fechaDesde; // Ej: 2007-04-05T12:30-02:00
+	private LocalDate fechaHasta; // Ej: 2007-04-05T12:30-02:00
+	private String estado; // INACTIVO, ACTIVO
+	private float precio;
+	private String descripcion;
+	private String politicaCancelacion;
+	private List<String> servicios;
+	private List<String> mediosDePago;
+
+	public int getIdPaquete() {
+		return idPaquete;
+	}
+
+	public void setIdPaquete(int idPaquete) {
+		this.idPaquete = idPaquete;
+	}
+
+	public String getNombrePaquete() {
+		return nombrePaquete;
+	}
+
+	public void setNombrePaquete(String nombrePaquete) {
+		this.nombrePaquete = nombrePaquete;
+	}
+
+	public int getIdCiudadDestino() {
+		return idCiudadDestino;
+	}
+
+	public void setIdCiudadDestino(int idCiudadDestino) {
+		this.idCiudadDestino = idCiudadDestino;
+	}
+
+	public String getNombreCiudadDestino() {
+		return nombreCiudadDestino;
+	}
+
+	public void setNombreCiudadDestino(String nombreCiudadDestino) {
+		this.nombreCiudadDestino = nombreCiudadDestino;
+	}
+
+	public int getCupo() {
+		return cupo;
+	}
+
+	public void setCupo(int cupo) {
+		this.cupo = cupo;
+	}
+
+	public int getCantPersonas() {
+		return cantPersonas;
+	}
+
+	public void setCantPersonas(int cantPersonas) {
+		this.cantPersonas = cantPersonas;
+	}
+
+	public int getIdAgencia() {
+		return idAgencia;
+	}
+
+	public void setIdAgencia(int idAgencia) {
+		this.idAgencia = idAgencia;
+	}
+
+	public String getNombreAgencia() {
+		return nombreAgencia;
+	}
+
+	public void setNombreAgencia(String nombreAgencia) {
+		this.nombreAgencia = nombreAgencia;
+	}
+
+	public String getDireccionAgencia() {
+		return direccionAgencia;
+	}
+
+	public void setDireccionAgencia(String direccionAgencia) {
+		this.direccionAgencia = direccionAgencia;
+	}
+
+	public String getEstadoAgencia() {
+		return estadoAgencia;
+	}
+
+	public void setEstadoAgencia(String estadoAgencia) {
+		this.estadoAgencia = estadoAgencia;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public LocalDate getFechaDesde() {
+		return fechaDesde;
+	}
+
+	public void setFechaDesde(LocalDate fechaDesde) {
+		this.fechaDesde = fechaDesde;
+	}
+
+	public LocalDate getFechaHasta() {
+		return fechaHasta;
+	}
+
+	public void setFechaHasta(LocalDate fechaHasta) {
+		this.fechaHasta = fechaHasta;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getPoliticaCancelacion() {
+		return politicaCancelacion;
+	}
+
+	public void setPoliticaCancelacion(String politicaCancelacion) {
+		this.politicaCancelacion = politicaCancelacion;
+	}
+
+	public List<String> getServicios() {
+		return servicios;
+	}
+
+	public void setServicios(List<String> servicios) {
+		this.servicios = servicios;
+	}
+
+	public List<String> getMediosDePago() {
+		return mediosDePago;
+	}
+
+	public void setMediosDePago(List<String> mediosDePago) {
+		this.mediosDePago = mediosDePago;
+	}
 }
