381359aca96e2590721c990a41df0a1566f68072
Walter <Walter@192.168.0.10>
2018-06-20 20:45:24 -0300

    logging action

--- TurismoApp/ejbModule/com/turismo/backoffice/logging/LoggingAccion.java
+++ TurismoApp/ejbModule/com/turismo/backoffice/logging/LoggingAccion.java
@@ -2,9 +2,9 @@ package com.turismo.backoffice.logging;
 
 public enum LoggingAccion {
 	RESERVA_DE_HOTEL(1, "Nueva reserva de hotel"),
-	RESERVA_DE_PAQUETE(2, "Reserva Paquete"),
-	NUEVA_PUBLICACION_OFERTA_HOTELERA(3, "Registro Oferta Hotelera"),
-	NUEVA_PUBLICACION_OFERTA_PAQUETE(4, "Registro Oferta Paquete"),
+	RESERVA_DE_PAQUETE(2, "Nueva reserva Paquete"),
+	NUEVA_PUBLICACION_OFERTA_HOTELERA(3, "Registro de Oferta Hotelera en el portal web"),
+	NUEVA_PUBLICACION_OFERTA_PAQUETE(4, "Registro Oferta Paquete en el portal web"),
 	BUSQUEDA_OFERTA_HOTELERA(5, "Buscar Oferta Hotelera"),
 	BUSQUEDA_OFERTA_PAQUETE(6, "Buscar Oferta Paquete"),
 	VISUALIZAR_DETALLE_OFERTA_HOTELERA(7, "Ver Detalle Oferta Hotelera"),
