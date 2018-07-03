16317d3d48c248070d92c1b1852209cd8ef64f03
walter <wr.ariel@gmail.com>
2018-07-02 21:59:30 -0300

    Archivo SOAP_1.wsdl esta contenido en este proyecto.

---
+++ GRUPO_05_BACKOFFICE/src/com/backoffice/servicios/EstaAutorizado.java
@@ -0,0 +1,62 @@
+
+package com.backoffice.servicios;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para estaAutorizado complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="estaAutorizado"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="codEntidad" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "estaAutorizado", propOrder = {
+    "codEntidad"
+})
+public class EstaAutorizado {
+
+    @XmlElement(required = true)
+    protected String codEntidad;
+
+    /**
+     * Obtiene el valor de la propiedad codEntidad.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCodEntidad() {
+        return codEntidad;
+    }
+
+    /**
+     * Define el valor de la propiedad codEntidad.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCodEntidad(String value) {
+        this.codEntidad = value;
+    }
+
+}
---
+++ GRUPO_05_BACKOFFICE/src/com/backoffice/servicios/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://servicios.backoffice.com/")
+package com.backoffice.servicios;
